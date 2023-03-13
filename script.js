7e6681db625da9e69b28a4282868378d1c869344
Matt <124236404+SensuBeanz@users.noreply.github.com>
2023-03-13 13:49:46 +1100

    Playround complete and score incrementor

--- script.js
+++ script.js
@@ -14,14 +14,62 @@ function getComputerChoice() {
     return compchoice;
 }
 
-console.log(getComputerChoice())
-
 //player selection input
 function getPlayerChoice() {
     let input = prompt("Player Selection");
-    let input = input.toLowerCase();
-    return input;
+    let choice = input.toLowerCase();
+    const firstlet = choice.charAt(0);
+    const firstLetUp = firstlet.toUpperCase();
+    const word = choice.substr(1);
+    const finalChoice = firstLetUp + word;
+    if (finalChoice === "Rock" || finalChoice === "Paper" || finalChoice === "Scissors") {
+        return finalChoice;
+    } else {
+        let invalidChoiceRun = getPlayerChoiceError();
+        return invalidChoiceRun;
+    }
 }
 
-console.log(getPlayerChoice())
+function getPlayerChoiceError() {
+    let input = prompt("Please select either Rock, Paper or Scissors");
+    let choice = input.toLowerCase();
+    const firstlet = choice.charAt(0);
+    const firstLetUp = firstlet.toUpperCase();
+    const word = choice.substr(1);
+    const finalChoice = firstLetUp + word;
+    if (finalChoice === "Rock" || finalChoice === "Paper" || finalChoice === "Scissors") {
+        return finalChoice;
+    } else {
+        getPlayerChoiceError()
+    }
+}
+
 //play game - player,computer
+function playRound() {
+    const computer = getComputerChoice();
+    const player = getPlayerChoice();
+    console.log("Player: " + player + ", Computer: " + computer)
+    switch (player) {
+        case "Rock":
+        switch (computer) {
+            case "Rock":
+                console.log("The round is a tie!");
+                break;
+            case "Paper":
+                console.log("Computer wins this round.");
+                computerScore++;
+                break;
+            case "Scissors":
+                console.log("You win this round!");
+                playerScore++;
+                break;
+        }
+        break;
+    }
+    console.log("Player " + playerScore + ", Computer " + computerScore)
+}
+
+let playerScore = 0
+let computerScore = 0
+
+playRound()
